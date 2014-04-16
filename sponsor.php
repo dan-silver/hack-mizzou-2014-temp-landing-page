d6c8f55dcc3d976ae5aa53985695ba505591f689
Dan Silver <das2c3@mail.missouri.edu>
2014-04-15 19:43:47 -0500

    a

--- sponsor.php
+++ sponsor.php
@@ -1,9 +1,11 @@
 <?php
 
-$file_url = 'http://2014.hackmizzou.com/SponsorGuide.pdf';
-header('Content-Type: application/octet-stream');
-header("Content-Transfer-Encoding: Binary"); 
-header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\""); 
-readfile($file_url);
+// $file_url = 'http://2014.hackmizzou.com/SponsorGuide.pdf';
+// header('Content-Type: application/octet-stream');
+// header("Content-Transfer-Encoding: Binary"); 
+// header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\""); 
+// readfile($file_url);
+
+echo "download sponsor doc!";
 
 ?>
